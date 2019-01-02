87a05d9751398d08a965f24bbe9e3f11e5d69b9b
imdjs <imdjs@hotmail.com>
2019-01-02 10:08:33 +0800

    Add files via upload

---
+++ addons/rotbone/draw.gd
@@ -0,0 +1,24 @@
+tool
+extends CanvasItem
+
+func _ready():
+    pass
+
+func _process(delta):
+    update()
+    
+func _draw():
+    var pos = get_viewport().get_rect().size/2 #get center of the screen
+    var rect_size = Vector2(80,60) #the size of your rectangle
+
+    #because the rectangle origin is his top-left
+    #does some correction to make it centered
+    var rect_pos = pos - rect_size/2 
+
+    #create your Rect2
+    var rect = Rect2(rect_pos, rect_size )
+
+    #drawn Rect
+    draw_rect( rect, Color(1,1,1) )
+    print("_draw");
+    #drawn circle (origin is center by default)
